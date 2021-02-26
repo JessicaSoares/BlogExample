dc0aa2f39795167b1268976943b6a3fcb9b5a48b
jess <jessicassoarez@gmail.com>
2021-02-26 17:56:25 -0300

    first commit

---
+++ header.php
@@ -0,0 +1,8 @@
+ 
+<link href="http://localhost/blog/assets/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<ul class="ulclass">
+<li><a href="http://localhost/blog/">Home>/a<</li>
+
+</ul>
